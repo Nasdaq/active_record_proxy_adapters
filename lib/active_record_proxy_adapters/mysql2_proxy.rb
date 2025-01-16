ebf47c30a0db6a77eca9e79d127da7bd17a30d48
Matt Cruz <matt.cruz@nasdaq.com>
2025-01-16 13:04:06 -0500

    Remove unnecessary require

--- lib/active_record_proxy_adapters/mysql2_proxy.rb
+++ lib/active_record_proxy_adapters/mysql2_proxy.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require "active_record_proxy_adapters/primary_replica_proxy"
-require "active_record_proxy_adapters/active_record_context"
 
 module ActiveRecordProxyAdapters
   # Proxy to the original Mysql2Adapter, allowing the use of the ActiveRecordProxyAdapters::PrimaryReplicaProxy.
