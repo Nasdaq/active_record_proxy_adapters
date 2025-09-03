57f0c2531a86f74c3e98d5c6be6c473b2f9fe819
Matt Cruz <matt.cruz@nasdaq.com>
2025-09-03 13:08:17 -0400

    Move loading of TrilogyProxy to its own railtie

---
+++ lib/active_record_proxy_adapters/railties/trilogy.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "active_support"
+
+module ActiveRecordProxyAdapters
+  module Railties
+    # Hooks into rails boot process to load the Trilogy Proxy adapter.
+    class TrilogyProxy < Rails::Railtie
+      require "active_record_proxy_adapters/connection_handling/trilogy_proxy"
+    end
+  end
+end
