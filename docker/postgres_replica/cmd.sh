57d0343c6d6e75f5ebf0011c2065376212ad9941
Matt Cruz <matt.cruz@nasdaq.com>
2025-10-03 21:58:28 -0400

    Add volumes for postgres containers

--- docker/postgres_replica/cmd.sh
+++
@@ -1,9 +0,0 @@
-until pg_basebackup --pgdata=/var/lib/postgresql/data -R --slot=$PRIMARY_REPLICATION_SLOT --host=$PRIMARY_DATABASE_HOST --port=$PRIMARY_DATABASE_PORT
-do
-  echo 'Waiting for primary to connect...'
-  sleep 1s
-done
-
-echo 'Backup done, starting replica...'
-chmod 0700 /var/lib/postgresql/data
-postgres
